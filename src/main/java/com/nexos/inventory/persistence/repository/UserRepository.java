e672ce550173bc41ae7de866877dac2d1c07e3f9
RichardCBravo <richardkbravo@gmail.com>
2025-07-06 14:22:00 -0500

    Add Merchandise and User repository interfaces

---
+++ src/main/java/com/nexos/inventory/persistence/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nexos.inventory.persistence.repository;
+
+import com.nexos.inventory.persistence.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
